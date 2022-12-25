0de4c0b73eae5d68a4f0665b7925174116fc952d
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2022-12-25 23:57:32 +0530

    add input from stdin and refactor

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+
+#define MESSAGE_BUFSIZE 1024
 
 char* letters[26] = {
     "._",   //A
@@ -38,6 +42,43 @@ char* letters[26] = {
     "__.."  //Z
 };
 
+void encode(){
+    char c;
+    int position = 0;
+    int message_size = MESSAGE_BUFSIZE;
+    char *message = malloc(sizeof(char) * message_size);
+
+    if (!message) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    while(1){
+        c = getchar();
+
+        if (c == EOF || c == '\n') {
+          message[position] = '\0';
+          for(int j = 0; j < position; j++){
+              if((int)message[j] > 90){
+                  message[j] = message[j] - 32;
+              }
+              if(message[j] == 32){
+                  printf(" /");
+              }else{
+                  printf("%s/", letters[(int)message[j] - 65]);
+              }
+          }
+          printf("\n");
+          return;
+          // return message;
+        } else {
+          message[position] = c;
+        }
+        position++;
+    }
+}
+
 void help(char** programname){
     printf("Morse code generator/translator \n");
     printf("USAGE: %s [OPTION] [FILE]\n", *programname);
@@ -46,26 +87,15 @@ void help(char** programname){
 int main (int argc, char *argv[]){
 
     if (argc < 2) {
+        fprintf(stderr, "Arguments not provided!\n");
         help(argv);
-        return -1;
+        return 1;
     }
 
-    if ( !strcmp(argv[1], "code") ) {
-
-        char string[10];
-        for(int i = 0; i < 10; i++){
-            scanf(" %c", &string[i]);
-        }
-        for(int i = 0; i < 10; i++){
-            if((int)string[i] > 90){
-                string[i] = string[i] - 32;
-            }
-            printf("%s/", letters[(int)string[i] - 65]);
-        }
-        printf("\n");
-
+    if ( !strcmp(argv[1], "encode") ) {
+        encode();
     }else if ( !strcmp(argv[1], "decode") ) {
-        printf("bruh\n");
+        printf("decode\n");
     }
 
     return 0;
